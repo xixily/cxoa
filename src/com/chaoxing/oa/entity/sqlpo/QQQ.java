ae4ea8211b90e2f6156cb59025e9a8efbe88bdb6
dengxf <570341093@qq.com>
2016-10-09 18:46:21 +0800

    提交测试2

---
+++ src/com/chaoxing/oa/entity/sqlpo/QQQ.java
@@ -0,0 +1,27 @@
+package com.chaoxing.oa.entity.sqlpo;
+
+public class QQQ {
+
+	private int id;
+	private String qq;
+	private String op;
+	public int getId() {
+		return id;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public String getOp() {
+		return op;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	public void setOp(String op) {
+		this.op = op;
+	}
+	
+}
