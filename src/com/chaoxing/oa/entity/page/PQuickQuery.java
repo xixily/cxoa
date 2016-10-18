65053cb51924ea7a63c03411482afc59b6e4ff83
dengxf <570341093@qq.com>
2016-10-18 12:04:31 +0800

    快捷查询page

---
+++ src/com/chaoxing/oa/entity/page/PQuickQuery.java
@@ -0,0 +1,45 @@
+package com.chaoxing.oa.entity.page;
+
+public class PQuickQuery {
+	private int type;
+	/**
+	 * 分页项
+	 */
+	private int page;
+	private int rows;
+	private String sort;
+	private String order;
+	public int getType() {
+		return type;
+	}
+	public int getPage() {
+		return page;
+	}
+	public int getRows() {
+		return rows;
+	}
+	public String getSort() {
+		return sort;
+	}
+	public String getOrder() {
+		return order;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+	public void setOrder(String order) {
+		this.order = order;
+	}
+
+
+	
+}
