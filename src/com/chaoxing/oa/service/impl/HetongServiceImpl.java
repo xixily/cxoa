71c4e5b478db73c63742e7d33c383247439fd70a
dengxf <570341093@qq.com>
2016-10-24 15:41:41 +0800

    合同发快递serviceImple

--- src/com/chaoxing/oa/service/impl/HetongServiceImpl.java
+++ src/com/chaoxing/oa/service/impl/HetongServiceImpl.java
@@ -7,15 +7,21 @@ import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import com.chaoxing.oa.config.SysConfig;
 import com.chaoxing.oa.dao.BaseHetongDaoI;
+import com.chaoxing.oa.entity.page.Json;
+import com.chaoxing.oa.entity.page.SF.Cargo;
+import com.chaoxing.oa.entity.page.SF.Order;
+import com.chaoxing.oa.entity.page.SF.OrderResponse;
 import com.chaoxing.oa.entity.page.hetong.PFahuo;
 import com.chaoxing.oa.entity.page.hetong.PFapiao;
 import com.chaoxing.oa.entity.sqlpo.FaPiao;
 import com.chaoxing.oa.entity.sqlpo.Fahuo;
 import com.chaoxing.oa.service.HetongService;
+import com.chaoxing.oa.util.SFUtil;
 @Service("hetongService")
 public class HetongServiceImpl implements HetongService {
 	private BaseHetongDaoI<Fahuo> htDao;
@@ -82,16 +88,52 @@ public class HetongServiceImpl implements HetongService {
 	
 	
 	@Override
-	public int sendKuaidi(PFahuo pfahuo) {
-		
-		return 0;
+	public OrderResponse sendKuaidi(PFahuo pfahuo) {
+//		PFahuo pf = new PFahuo();
+//		Integer a = (int) (Math.random()*10000000);
+//		pf.setOrderid(a);
+//		pf.setArea("0796");
+//		pf.setAreaCode("0796");
+//		pf.setContent("就是想发个快递");
+//		pf.setD_address("师范大学（瑶湖校区）邓 17744543034");
+//		pf.setD_company("师范大学");
+//		pf.setD_contact("小邓");
+//		pf.setD_tel("18146612837");
+//		pf.setD_post_code("330022");
+//		pf.setD_city("南昌");
+//		pf.setRemark("这是一个很长很长的故事i·~~");
+//		pf.setSender("邓~");
+		Order order = new Order();
+		BeanUtils.copyProperties(pfahuo, order);
+		order.setJ_tel("17744543034");
+		order.setJ_company("世纪超星公司");
+		order.setJ_contact("小邓");
+		order.setJ_address("北京市海淀区 上地东里3区4号楼 601");
+		Cargo cargo = new Cargo();
+		cargo.setName("发票");
+		List<Cargo> cargos = new ArrayList<Cargo>();
+		cargos.add(cargo);
+		Json result = SFUtil.sendOrder(order, cargos, null);
+//		if(result.isSuccess()){
+//			OrderResponse or = (OrderResponse) result.getObj();
+//			String mailno = or.getMailno();
+//			or.getOrderid();
+//			BarCode128C.getCode128CPicture(mailno, 22, "d:/code3.jpg");
+//		}
+//		System.out.println(result.getMsg());
+		//查询订单 6320318
+//		Json result2 = s.queryOrder(String.valueOf(pf.getOrderid()));
+//		Json result2 = s.queryOrder("4624347");
+//		System.out.println((OrderResponse)result2.getObj());
+		Json result3 = SFUtil.queryRoute("4624347");
+		System.out.println(result3.getMsg());
+		System.out.println("路由查询结果" + (OrderResponse)result3.getObj());
+		return null;
 	}
 	/*private void addConditions(QueryForm queryForm, StringBuffer hql, Map<String, Object> params) {
 	
 	}*/
 	/*@Override
-	 * (non-Javadoc)
-	 * @see com.chaoxing.oa.service.HetongService#findFapiao(com.chaoxing.oa.entity.page.hetong.PFapiao, int)
 	 */
 	public Map<String, Object> findFapiao(PFapiao pfapiao, int isExport) {
 		StringBuffer hql = new StringBuffer("from FaPiao t where t.money > 0 ");
@@ -155,6 +197,13 @@ public class HetongServiceImpl implements HetongService {
 		StringBuffer hql = new StringBuffer("update FaPiao t set t.queryStatus = :queryStatus where id = :id");
 		int i = fpDao.executeHql(hql.toString(), params);
 		return i;
+	}
+	@Override
+	public PFahuo getFahuo(int orderId) {
+		Fahuo fahuo = htDao.get(Fahuo.class, orderId);
+		PFahuo pfahuo = new PFahuo();
+		BeanUtils.copyProperties(fahuo, pfahuo);
+		return pfahuo;
 	}
 	
 }
