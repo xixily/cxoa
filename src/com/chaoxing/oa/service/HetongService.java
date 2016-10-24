75be847cd9de2a7f2e644b7f40f08ce82e4d1ee3
dengxf <570341093@qq.com>
2016-10-24 15:41:00 +0800

    合同发快递service

--- src/com/chaoxing/oa/service/HetongService.java
+++ src/com/chaoxing/oa/service/HetongService.java
@@ -2,15 +2,18 @@ package com.chaoxing.oa.service;
 
 import java.util.Map;
 
+import com.chaoxing.oa.entity.page.SF.OrderResponse;
 import com.chaoxing.oa.entity.page.hetong.PFahuo;
 import com.chaoxing.oa.entity.page.hetong.PFapiao;
 
 public interface HetongService {
 	public Map<String, Object> findHetong(PFahuo pfahuo, int isExport);
 
-	public int sendKuaidi(PFahuo pfahuo);
+	public OrderResponse sendKuaidi(PFahuo pfahuo);
 
 	public Map<String, Object> findFapiao(PFapiao pfapiao, int isExport);
 
 	public int updateFapiao(PFapiao pfapiao);
+
+	public PFahuo getFahuo(int orderId);
 }
